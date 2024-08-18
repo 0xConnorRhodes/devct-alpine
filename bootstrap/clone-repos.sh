efcdd1bfcb446764952d83c793889407ce2054b4
Connor Rhodes <connor@rhodes.contact>
2024-08-18 15:15:16 +0000

    update clone-repos

--- bootstrap/clone-repos.sh
+++ bootstrap/clone-repos.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-git clone git@github.com:0xConnorRhodes/notes-scripts.git $HOME/code/notes-scripts
 git clone git@github.com:0xConnorRhodes/priv-scripts.git $HOME/code/priv-scripts
 git clone git@github.com:0xConnorRhodes/notes-snapshot.git $HOME/notes
+
+git clone git@github.com:0xConnorRhodes/notes-scripts.git $HOME/code/notes-scripts
+cd $HOME/code/notes-scripts
+just pull-sub
