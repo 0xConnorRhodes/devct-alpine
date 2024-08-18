f319f2538818968ba4914119cd71e73a775c0240
Connor Rhodes <connor@rhodes.contact>
2024-08-18 14:42:14 +0000

    added code to symlink notes mount to home directory

--- bootstrap/entrypoint.sh
+++ bootstrap/entrypoint.sh
@@ -4,5 +4,12 @@
 chezmoi apply
 chezmoi apply
 
+# symlink notes mount to home directory if it exists
+if [ ! -e "$HOME/notes" ]; then
+  if [ -d "/nmount/zk_notes" ]; then
+    ln -s /nmount/zk_notes "$HOME/notes"
+  fi
+fi
+
 # infinite task to hold container open so shell sessions can be attached
 sh -c "tail -f /dev/null"
