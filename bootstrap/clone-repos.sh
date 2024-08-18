0d362a2d820ce91c0f5bfa1e3ef27b5b77937b9c
Connor Rhodes <connor@rhodes.contact>
2024-08-18 14:51:32 +0000

    add clone-repos script for setup and update entrypoint

---
+++ bootstrap/clone-repos.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git clone git@github.com:0xConnorRhodes/notes-scripts.git $HOME/code/notes-scripts
+git clone git@github.com:0xConnorRhodes/priv-scripts.git $HOME/code/priv-scripts
+git clone git@github.com:0xConnorRhodes/notes-snapshot.git $HOME/notes
