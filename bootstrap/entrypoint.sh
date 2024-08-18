b087ff0cba69e8581da0eef97ec6a1a750fd6c74
Connor Rhodes <connor@rhodes.contact>
2024-08-18 14:13:39 +0000

    fix chezmoi apply in entrypoint

--- bootstrap/entrypoint.sh
+++ bootstrap/entrypoint.sh
@@ -2,6 +2,7 @@
 
 # second apply to decrypt files if key is present
 chezmoi apply
+chezmoi apply
 
 # infinite task to hold container open so shell sessions can be attached
 sh -c "tail -f /dev/null"
